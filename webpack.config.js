042d80517b8d1388b35d7657b6423b990322cd2f
Kim Kwanka <kimkwanka@gmail.com>
2017-02-20 15:17:17 +0100

    Moved merged client and server webpack config into one file

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const debug = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const path = require('path');
+const fs = require('fs');
 
-const BUILD_DIR = path.resolve(__dirname, 'public/');
+const SERVER_BUILD_DIR = path.resolve(__dirname, './');
+const CLIENT_BUILD_DIR = path.resolve(__dirname, 'public/');
 const APP_DIR = path.resolve(__dirname, 'src/');
 
-const config = {
+const configClient = {
+  name: 'client',
   entry: path.join(APP_DIR, 'client.js'),
   devtool: debug ? 'inline-sourcemap' : null,
   output: {
-    path: BUILD_DIR,
+    path: CLIENT_BUILD_DIR,
     filename: 'bundle.js',
     publicPath: '/',
   },
@@ -29,4 +32,36 @@ const config = {
   ],
 };
 
-module.exports = config;
+const configServer = {
+  name: 'server',
+  entry: path.join(APP_DIR, 'server.js'),
+
+  output: {
+    path: SERVER_BUILD_DIR,
+    filename: 'server.bundle.js',
+  },
+
+  target: 'node',
+
+  // keep node_module paths out of the bundle
+  externals: fs.readdirSync(path.resolve(__dirname, 'node_modules')).concat([
+    'react-dom/server', 'react/addons',
+  ]).reduce((ext, mod) => {
+    const tmp = ext;
+    tmp[mod] = `commonjs ${mod}`;
+    return tmp;
+  }, {}),
+
+  node: {
+    __filename: true,
+    __dirname: true,
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
+    ],
+  },
+};
+
+module.exports = [configClient, configServer];
