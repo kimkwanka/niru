04dc1c70576d2689694de8e80fb7b2a5802cc6b3
Kim Kwanka <kimkwanka@gmail.com>
2017-06-14 14:26:53 +0200

    Simplify Route creation and upgrade all packages.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,27 +6,25 @@ import Header from './pages/shared/Header';
 
 import routesArr from '../shared/routes';
 
-const App = (props) => {
+const App = ({ serverMatch }) => {
 // If <App /> is rendered on the server we need to provide the serverMatch prop
 // since StaticRouter can only render a single Route (Switch only works on client side).
 // On the client though, just return all routes and let Switch do the work.
 
-  const match = props.serverMatch;
+  const routes = serverMatch ?
+    <Route key={serverMatch.path} {...serverMatch} /> :
+    routesArr.map(route => <Route key={route.path} {...route} />);
 
-  const routes = !match ? routesArr.map(({ path, exact, component }, index) => (
-      createElement(Route, { path, exact, component, key: index })
-    )) : createElement(Route, {
-      path: match.path, exact: match.exact, component: match.component,
-    });
-
-  return (<div>
-    <Header />
-    <main>
-      <Switch>
-        {routes}
-      </Switch>
-    </main>
-  </div>);
+  return (
+    <div>
+      <Header />
+      <main>
+        <Switch>
+          {routes}
+        </Switch>
+      </main>
+    </div>
+  );
 };
 
 App.propTypes = {
