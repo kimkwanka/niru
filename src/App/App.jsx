9dcf74f2e031c0c8909c65a8e27fb31015f87da0
Kim Kwanka <kimkwanka@gmail.com>
2019-01-30 23:45:53 +0100

    Use HashRouter until SSR is implemented.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from '../Home/Home';
 import About from '../About/About';
