b4d1d316f33eff9b2b7d5600f37a9165f3ab7f27
Kim Kwanka <kimkwanka@gmail.com>
2019-01-31 17:44:58 +0100

    Move components into /components.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 /* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App';
+import App from './components/App/App';
 
 const render = (Component) => {
   ReactDOM.render(
