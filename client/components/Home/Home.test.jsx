cbacb64a3fcb7631d8cdaa970fbcc7fca0b25a6a
Kim Kwanka <kimkwanka@gmail.com>
2019-02-11 03:05:18 +0100

    Add more tests for <Home />.

--- client/components/Home/Home.test.jsx
+++ client/components/Home/Home.test.jsx
@@ -5,28 +5,38 @@ import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import ConnectedHome, { Home } from './Home';
 
-const testUser = {
+const user = {
   name: 'TestUsername',
   authenticated: true,
 };
 
+const dispatch = () => {};
+
 describe('<Home /> - Snapshot', () => {
   it('matches its snapshot', () => {
-    const wrapper = shallow(<Home user={testUser} />);
+    const wrapper = shallow(<Home {...{ user, dispatch }} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
 
 describe('<Home /> - Shallow render undecorated component', () => {
   it('renders without exploding', () => {
-    const wrapper = shallow(<Home user={testUser} />);
+    const wrapper = shallow(<Home {...{ user, dispatch }} />);
     expect(wrapper).toHaveLength(1);
+  });
+  it('renders the user name', () => {
+    const wrapper = shallow(<Home {...{ user, dispatch }} />);
+    expect(wrapper.find('h4 span').at(0).text()).toBe(' TestUsername');
+  });
+  it('renders the user authentication status', () => {
+    const wrapper = shallow(<Home {...{ user, dispatch }} />);
+    expect(wrapper.find('h4 span').at(1).text()).toBe(' true');
   });
 });
 
 describe('<Home /> - Mount Redux connected component', () => {
   const mockStore = configureStore();
-  const initialState = { user: testUser };
+  const initialState = { user };
 
   let store = null;
   let wrapper = null;
@@ -42,5 +52,26 @@ describe('<Home /> - Mount Redux connected component', () => {
 
   it('renders without exploding', () => {
     expect(wrapper.find(Home)).toHaveLength(1);
+  });
+
+  it('handles input change', () => {
+    expect(store.getActions().length).toBe(0);
+    wrapper.find('input').simulate('change', {
+      target: { value: 'NewUserName' },
+    });
+    expect(store.getActions().length).toBe(1);
+    expect(store.getActions()[0]).toEqual({
+      type: 'RENAME_USER',
+      name: 'NewUserName',
+    });
+  });
+
+  it('handles clicks', () => {
+    expect(store.getActions().length).toBe(0);
+    wrapper.find('button').simulate('click');
+    expect(store.getActions().length).toBe(1);
+    expect(store.getActions()[0]).toEqual({
+      type: 'TOGGLE_AUTHENTICATED',
+    });
   });
 });
