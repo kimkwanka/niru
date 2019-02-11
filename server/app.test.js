52a701a1d8bd8145d4515a0c544d9973b793bd5b
Kim Kwanka <kimkwanka@gmail.com>
2019-02-11 22:54:36 +0100

    Make express server tests cover both process.env.NODE_ENV branches.

--- server/app.test.js
+++ server/app.test.js
@@ -1,41 +1,110 @@
 /**
  * @jest-environment node
  */
+/* eslint-disable global-require */
 import request from 'supertest';
 
-import app from './app';
+// Test process.env code adapted from:
+// https://stackoverflow.com/questions/48033841/test-process-env-with-jest#48042799
+
+let app = null;
 
 describe('Express Server', () => {
-  it('/api responds with json response', () => request(app)
-    .get('/api')
-    .expect('Content-Type', /json/)
-    .expect('Content-Length', '21')
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toEqual({
-        data: 'Some data!',
-      });
-    }));
+  describe('process.env.NODE_ENV = test', () => {
+    const OLD_ENV = process.env;
 
-  it('serves static assets from the public folder', () => request(app)
-    .get('/favicon.ico')
-    .expect('Content-Type', 'image/x-icon')
-    .expect('Content-Length', '1150')
-    .expect(200));
+    beforeEach(() => {
+      jest.resetModules(); // this is important
+      process.env = { ...OLD_ENV };
+      delete process.env.NODE_ENV;
+      process.env.NODE_ENV = 'test';
+      app = require('./app').default;
+    });
 
-  it('handles /', () => request(app)
-    .get('/')
-    .expect(200));
+    afterEach(() => {
+      process.env = OLD_ENV;
+    });
 
-  it('handles /about', () => request(app)
-    .get('/about')
-    .expect(200));
+    it('/api responds with json response', () => request(app)
+      .get('/api')
+      .expect('Content-Type', /json/)
+      .expect('Content-Length', '21')
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual({
+          data: 'Some data!',
+        });
+      }));
 
-  it('handles /styleguide', () => request(app)
-    .get('/styleguide')
-    .expect(200));
+    it('serves static assets from the public folder', () => request(app)
+      .get('/favicon.ico')
+      .expect('Content-Type', 'image/x-icon')
+      .expect('Content-Length', '1150')
+      .expect(200));
 
-  it('404s everything else', () => request(app)
-    .get('/everythingelseis404ed')
-    .expect(404));
+    it('handles /', () => request(app)
+      .get('/')
+      .expect(200));
+
+    it('handles /about', () => request(app)
+      .get('/about')
+      .expect(200));
+
+    it('handles /styleguide', () => request(app)
+      .get('/styleguide')
+      .expect(200));
+
+    it('404s everything else', () => request(app)
+      .get('/everythingelseis404ed')
+      .expect(404));
+  });
+
+  describe('process.env.NODE_ENV = production', () => {
+    const OLD_ENV = process.env;
+
+    beforeEach(() => {
+      jest.resetModules(); // this is important
+      process.env = { ...OLD_ENV };
+      delete process.env.NODE_ENV;
+      process.env.NODE_ENV = 'production';
+      app = require('./app').default;
+    });
+
+    afterEach(() => {
+      process.env = OLD_ENV;
+    });
+
+    it('/api responds with json response', () => request(app)
+      .get('/api')
+      .expect('Content-Type', /json/)
+      .expect('Content-Length', '21')
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual({
+          data: 'Some data!',
+        });
+      }));
+
+    it('serves static assets from the public folder', () => request(app)
+      .get('/favicon.ico')
+      .expect('Content-Type', 'image/x-icon')
+      .expect('Content-Length', '1150')
+      .expect(200));
+
+    it('handles /', () => request(app)
+      .get('/')
+      .expect(200));
+
+    it('handles /about', () => request(app)
+      .get('/about')
+      .expect(200));
+
+    it('handles /styleguide', () => request(app)
+      .get('/styleguide')
+      .expect(200));
+
+    it('404s everything else', () => request(app)
+      .get('/everythingelseis404ed')
+      .expect(404));
+  });
 });
