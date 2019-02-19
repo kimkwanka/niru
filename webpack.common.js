0f2fa02f3d99e34dd20f795e63c09d4586d4d269
Kim Kwanka <kimkwanka@gmail.com>
2019-02-19 23:30:22 +0100

    docs: fix typo

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          // Disable transformation of ES6 module syntax to make enable Tree Shaking.
+          // Disable transformation of ES6 module syntax to enable Tree Shaking.
           // Note that we also need to set NODE_ENV to 'production' manually
           // (see package.json 'build' script)
           options: {
