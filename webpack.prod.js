9e9a73f9f03cdf90de7ccd6dbed3c9bfa782d806
kimkwanka <kimkwanka@gmail.com>
2021-05-12 20:12:49 +0200

    fix(webpack): import new webpack-merge correctly

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 const path = require('path');
 const glob = require('glob');
